04a0d9a3c0cb7093e9883ee65f4d471f69c200b0
RomainP <romainportier7@gmail.com>
2024-07-16 03:57:22 +0200

    add missing getters + user builder

--- src/main/java/com/rental/api/model/User.java
+++ src/main/java/com/rental/api/model/User.java
@@ -40,18 +40,85 @@ public class User implements UserDetails{
     @Column(name = "updated_at")
     private Date updatedAt;
 
+    private User() {
+        // Private constructor to force the use of builder
+    }
+
+    // Getters
+    public Integer getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
         return List.of();
     }
 
     @Override
-    public String getPassword() {
-        return password;
+    public String getUsername() {
+        return email;
+    }
+
+    public static class Builder {
+        private String name;
+        private String email;
+        private String password;
+
+        public Builder setName(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public Builder setEmail(String email) {
+            this.email = email;
+            return this;
+        }
+
+        public Builder setPassword(String password) {
+            this.password = password;
+            return this;
+        }
+
+        public User build() {
+            User user = new User();
+            user.name = this.name;
+            user.email = this.email;
+            user.password = this.password;
+            return user;
+        }
+
+    }
+
+    // TODO Usefull ?
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
     }
 
     @Override
-    public String getUsername() {
-        return email;
-    } 
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
