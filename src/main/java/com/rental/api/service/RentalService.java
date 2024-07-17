921a82358a1551a6597e3a6f2422247b7edf11d0
RomainP <romainportier7@gmail.com>
2024-07-17 16:17:31 +0200

    add rental details route and logic

--- src/main/java/com/rental/api/service/RentalService.java
+++ src/main/java/com/rental/api/service/RentalService.java
@@ -3,6 +3,7 @@ package com.rental.api.service;
 
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
@@ -21,16 +22,20 @@ import java.nio.file.Path;
 @Service
 public class RentalService {
     private final RentalRepository rentalRepository;
-
+    
     @Value("${file.upload-dir}")
     private String uploadDir;
-
+    
     public RentalService(RentalRepository rentalRepository){
         this.rentalRepository = rentalRepository;
     }
 
     public Iterable<Rental> getAllRentals() {
         return rentalRepository.findAll();
+    }
+
+    public Optional<Rental> getRentalById(final Integer id) {
+        return rentalRepository.findById(id);
     }
 
     public Rental addRental(RentalDto input){
@@ -81,4 +86,5 @@ public class RentalService {
             throw new RuntimeException("Failed to save file", e);
         }
     }
+
 }
