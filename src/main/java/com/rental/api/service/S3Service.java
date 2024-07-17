21db5fec16a0255f769f3a3a0b13caab1e6aa366
RomainP <romainportier7@gmail.com>
2024-07-17 18:34:16 +0200

    add Aws S3 SDK + S3 service/controller/configuration

---
+++ src/main/java/com/rental/api/service/S3Service.java
@@ -0,0 +1,45 @@
+package com.rental.api.service;
+
+import java.net.URL;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.AmazonS3Exception;
+
+import java.io.IOException;
+
+
+@Service
+public class S3Service {
+    private final AmazonS3 s3client;
+
+    @Value("${aws.s3.bucket}")
+    private String bucketName;
+
+    public S3Service(AmazonS3 s3client) {
+        this.s3client = s3client;
+    }
+
+    public String uploadFile(String keyName, MultipartFile file) {
+        try {
+            s3client.putObject(bucketName, keyName, file.getInputStream(), null);
+            return keyName;
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to read file input stream", e);
+        } catch (AmazonS3Exception e) {
+            throw new RuntimeException("Failed to upload file to S3", e);
+        }
+    }
+
+    public URL getFileUrl(String keyName) {
+        return s3client.getUrl(bucketName, keyName);
+    }
+
+    public URL uploadAndGetFileUrl(String keyName, MultipartFile file) {
+        uploadFile(keyName, file);
+        return getFileUrl(keyName);
+    }
+}
