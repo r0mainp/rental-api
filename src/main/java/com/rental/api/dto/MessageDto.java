d0874e0750c73ef85bc70927141fe0ba8b597538
RomainP <romainportier7@gmail.com>
2024-07-18 13:23:48 +0200

    add javadoc for dto package

--- src/main/java/com/rental/api/dto/MessageDto.java
+++ src/main/java/com/rental/api/dto/MessageDto.java
@@ -2,6 +2,9 @@ package com.rental.api.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+/**
+ * Data Transfer Object (DTO) representing a message to be sent or received.
+ */
 public class MessageDto {
     private String message;
 
@@ -11,30 +14,57 @@ public class MessageDto {
     @JsonProperty("user_id")
     private Integer userId;
 
+    /**
+     * Retrieves the message content.
+     *
+     * @return The message content
+     */
     public String getMessage() {
         return message;
     }
 
+    /**
+     * Sets the message content.
+     *
+     * @param message The message content to set
+     */
     public void setMessage(String message) {
         this.message = message;
     }
 
+    /**
+     * Retrieves the ID of the associated rental.
+     *
+     * @return The ID of the rental
+     */
     public Integer getRentalId() {
         return rentalId;
     }
 
+    /**
+     * Retrieves the ID of the associated user.
+     *
+     * @return The ID of the user
+     */
     public Integer getUserId() {
         return userId;
     }
 
+    /**
+     * Sets the ID of the associated rental.
+     *
+     * @param rentalId The ID of the rental to set
+     */
     public void setRentalId(Integer rentalId) {
         this.rentalId = rentalId;
     }
 
+    /**
+     * Sets the ID of the associated user.
+     *
+     * @param userId The ID of the user to set
+     */
     public void setUserId(Integer userId) {
         this.userId = userId;
     }
-
-    
-  
 }
--- src/main/java/com/rental/api/dto/RentalUpdateDto.java
+++ src/main/java/com/rental/api/dto/RentalUpdateDto.java
@@ -1,37 +1,83 @@
 package com.rental.api.dto;
 
+/**
+ * Data Transfer Object (DTO) representing the details required to update a rental.
+ */
 public class RentalUpdateDto {
-        private String name;
-        private int surface;
-        private double price;
-        private String description;
+    private String name;
+    private int surface;
+    private double price;
+    private String description;
 
-        public String getName() {
-            return name;
-        }
-        public int getSurface() {
-            return surface;
-        }
-        public double getPrice() {
-            return price;
-        }
-        public String getDescription() {
-            return description;
-        }
+    /**
+     * Retrieves the name of the rental property.
+     *
+     * @return The name of the rental
+     */
+    public String getName() {
+        return name;
+    }
 
-        
-        public void setName(String name) {
-            this.name = name;
-        }
-        public void setSurface(int surface) {
-            this.surface = surface;
-        }
-        public void setPrice(double price) {
-            this.price = price;
-        }
-        public void setDescription(String description) {
-            this.description = description;
-        }
+    /**
+     * Retrieves the surface area of the rental property.
+     *
+     * @return The surface area in square units
+     */
+    public int getSurface() {
+        return surface;
+    }
 
+    /**
+     * Retrieves the price of the rental property.
+     *
+     * @return The price per unit of time (e.g., per month)
+     */
+    public double getPrice() {
+        return price;
+    }
 
+    /**
+     * Retrieves the description of the rental property.
+     *
+     * @return The description of the rental
+     */
+    public String getDescription() {
+        return description;
+    }
+
+    /**
+     * Sets the name of the rental property.
+     *
+     * @param name The name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * Sets the surface area of the rental property.
+     *
+     * @param surface The surface area to set
+     */
+    public void setSurface(int surface) {
+        this.surface = surface;
+    }
+
+    /**
+     * Sets the price of the rental property.
+     *
+     * @param price The price to set
+     */
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    /**
+     * Sets the description of the rental property.
+     *
+     * @param description The description to set
+     */
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
