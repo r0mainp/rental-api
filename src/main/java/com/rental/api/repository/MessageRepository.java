b1ccda75945a3e7bc30c124c2d9de16762155d05
RomainP <romainportier7@gmail.com>
2024-07-18 12:54:28 +0200

    add javadoc for repository package

--- src/main/java/com/rental/api/repository/MessageRepository.java
+++ src/main/java/com/rental/api/repository/MessageRepository.java
@@ -5,7 +5,12 @@ import org.springframework.stereotype.Repository;
 
 import com.rental.api.model.Message;
 
+/**
+ * Repository interface for accessing Message entities in the database.
+ */
 @Repository
 public interface MessageRepository extends CrudRepository<Message, Integer>{
-
+    /**
+     * This interface inherits CRUD operations for Message entities.
+     */
 }
